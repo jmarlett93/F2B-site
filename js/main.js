db4f39e78a3132e6d62f0cecf11b5a148d7b84ab
John Marlett <1fiyero@gmail.com>
2016-09-11 14:52:34 -0500

    working on data fidelity

--- js/main.js
+++ js/main.js
@@ -14,29 +14,33 @@ function pageInitializer(){
         function queryPage(){
 
             if(document.body.id == "homepage"){
-                //call homepage functions
 
                 let formContent = setUpForm();
                 
             }else if(document.body.id == "contact-page"){
-                //call contactpage functions
-                registerButtons("submit");
+
+                let formContent = setUpForm();
+   
             }
             
             function processForm(){
-                let mailInputs = document.getElementsByClassName("form-control");
+                let htmlInputs = document.getElementsByClassName("form-control");
+                let mailInputs = [].forEach.call(htmlInputs, function(v,i,a){
+                    return htmlInputs[i];
+                })
+                console.log(mailInputs);             
                 return mailInputs;
-            }//process form
+            }
 
             function sendEmail(data){
-                 
+                
                 emailjs.send("gmail","template_bxGX1vPI",{first_name: data[0], last_name: data[1], email: data[2], phonenumber:data[3], address: data[4] })
                 .then(function(response) {
                     console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
                 }, function(err) {
                 console.log("FAILED. error=", err);
                 });
-            }  //sendEmail
+            }
             
             function setUpForm(){
                 let form = document.getElementById("email-form");
@@ -44,7 +48,6 @@ function pageInitializer(){
                     event.preventDefault();
                     let data = processForm();
                     console.log(data);
-
                     });
                 return form;
             }/*end function setUpForm */
